4f794253714c30353ed4badab6a2983bd73ec686
Yoan <70324016+syoanto21@users.noreply.github.com>
2026-03-06 23:26:29 +0700

    Add enableDuplicateUserCheck

--- src/config/app.js
+++ src/config/app.js
@@ -1,4 +1,5 @@
 export const APP_CONFIG = {
     enableEvents: false, // Set to false to disable event selection and always use global flow
     defaultEventId: 'global', // Default event ID to use when events are disabled
+    enableDuplicateUserCheck: false, // Set to false to allow multiple quiz attempts from the same user
 };
